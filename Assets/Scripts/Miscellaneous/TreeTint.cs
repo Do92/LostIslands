c94bbf8593aab732e973baca9551be1f879cf890
Do92 <docherie@yahoo.de>
2017-05-23 12:00:29 +0200

    New environment (no lightmap baking), connected tree material to healed tile percentage, new tree mesh.

---
+++ Assets/Scripts/Miscellaneous/TreeTint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+using UnityEngine.UI;
+
+public class TreeTint : MonoBehaviour {
+
+	public Material startMat;
+	public Material endMat;
+	private Renderer treeRenderer;
+	private Level level;
+	private float clamp;
+
+	void Start(){
+		treeRenderer = GetComponent<Renderer>();
+		treeRenderer.material = startMat;
+		level = FindObjectOfType<Level>();
+	}
+
+	void Update ()
+	{
+		treeRenderer.material.Lerp (startMat, endMat, ((float)level.healedTileCount / (float)level.tileCount));
+	}
+}
